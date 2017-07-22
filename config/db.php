7a16a1b9377e16e1791922bbfeb8b6ed3c588a09
BruceLi <lilong01@hecom.cn>
2017-07-22 08:31:09 +0800

    'init'

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=imooc_shop',
+    'username' => '4591009',
+    'password' => '123456',
+    'charset' => 'utf8',
+    'tablePrefix' => 'shop_',
+];
